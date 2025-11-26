5bf91a3572d45fa6c2ee5d8094b994ac96bc912f
juacc <juacc@juacc.net>
2025-11-26 11:13:45 -0300

    p4

--- p4/main.py
+++ p4/main.py
@@ -16,7 +16,7 @@ packs = {
     "enfriado":         make_dfs("./data/2 - 19-11/5 (enfriado)"),
 }
 
-# FAKE_ERROR = 0.1
+# FAKE_ERROR = 0.2
 FREQ = 6e-3
 
 
@@ -32,15 +32,15 @@ def make_model(t0, omega):
     def _model(t, kx, ex, *a):
         x_mov = t - t0
 
-        return np.exp(-ex) * np.cos(omega * x_mov - kx) + polynomial(x_mov, *a)
+        return np.exp(-ex) * np.sin(omega * x_mov - kx) + polynomial(x_mov, *a)
 
     return _model
 
 
-# fig, ax = plt.subplots(2, 1)
+fig, ax = plt.subplots(2, 1)
 
-es = []
-es_err = []
+exs = []
+exs_err = []
 kxs = []
 kxs_err = []
 
@@ -52,53 +52,52 @@ for ch, df in packs["osc"].items():
         make_model(t0, omega),
         ["kx", "ex", "a0", "a1", "a2"]
     )
-    c0 = df["Temperature"].max() - df["Temperature"].min()
+    # c0 = df["Temperature"].max() - df["Temperature"].min()
 
     # df["Error Temperature"] = FAKE_ERROR * df["Error Temperature"]
 
     fit_func = fit.fit(
         model,
         df,
-        p0=[0, 1.5, df["Temperature"].mean(), 0, 0]
+        p0=[0, 0, df["Temperature"].mean(), 0, 0]
     )
 
-    df_kx, c, *a = fit_func.param_val
-    kx_err, c_err, *a_err = fit_func.param_err
+    kx, ex, *a = fit_func.param_val
+    kx_err, ex_err, *a_err = fit_func.param_err
 
-    es.append(c)
-    es_err.append(c_err)
-    kxs.append(df_kx)
+    exs.append(ex)
+    exs_err.append(ex_err)
+    kxs.append(kx)
     kxs_err.append(kx_err)
 
-#     plot.datafit(
-#         df,
-#         fit_func,
-#         # datalabel=ch,
-#         ax=ax,
-#         no_yerr=True
-#     )
+    plot.datafit(
+        df,
+        fit_func,
+        datalabel=ch,
+        ax=ax,
+    )
 
-# ax[0].set(
-#     ylabel="Temperatura [C]"
-# )
-# ax[1].set(
-#     ylabel="Resiuos [C]",
-#     xlabel="Tiempo [s]"
-# )
-# plt.tight_layout()
-# plot.show()
+ax[0].set(
+    ylabel="Temperatura [C]"
+)
+ax[1].set(
+    ylabel="Resiuos [C]",
+    xlabel="Tiempo [s]"
+)
+plt.tight_layout()
+plot.show()
 
 df_ex = pd.DataFrame({
     "Distancia [mm]": dist,
     "Error dist": 0,
-    "epsion x": es,
-    "Error ex": es_err
+    "epsion x": exs,
+    "Error ex": exs_err
 })
 
 df_kx = pd.DataFrame({
     "Distancia [mm]": dist,
     "Error dist": 0,
-    "kx": kxs,
+    "kx": np.unwrap(kxs),
     "Error kx": kxs_err
 })
 
@@ -111,6 +110,20 @@ fit_func_k = fit.fit(
     df_kx
 )
 
+plot.datafit(
+    df_ex,
+    fit_func_epsilon,
+    datalabel="Epsilon X"
+)
+plot.show()
+
+plot.datafit(
+    df_kx,
+    fit_func_k,
+    datalabel="k X"
+)
+plot.show()
+
 epsilon = fit_func_epsilon.param_val[0]
 err_epsilon = fit_func_epsilon.param_err[0]
 k = fit_func_k.param_val[0]
@@ -119,9 +132,8 @@ err_k = fit_func_k.param_err[0]
 v = omega / k
 err_v = omega * err_k / k ** 2
 
-print(f"epsilon = {epsilon:.2e} +- {err_epsilon:.2e}")
-print(f"k = {k:.2e} +- {err_k:.2e}")
+# print(f"epsilon = {epsilon:.2e} +- {err_epsilon:.2e}")
+# print(f"k = {k:.2e} +- {err_k:.2e}")
 print(f"v = {v:.2e} +- {err_v:.2e} mm/s")
-
 print(f"kappa_e = {omega / (2 * epsilon**2):.2f} +- {err_epsilon * omega / epsilon**3:.2f}")
 print(f"kappa_v = {v / (2 * omega):.2f} +- {err_v * v / omega:.2f}")
